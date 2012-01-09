23f44a3592f6e740931cc758e0470dc8ec155608
Trond Arve Nordheim <tanordheim@gmail.com>
2012-01-09 10:44:59 +0100

    Adding min-height assignment to the content block when a content sidebar of content details block is present in the DOM to ensure its not flowing outside its bounds.

--- vendor/assets/javascripts/app-ui.js
+++ vendor/assets/javascripts/app-ui.js
@@ -6,15 +6,40 @@
 
 $(document).ready(function() {
 
+  var content = $('.content-container > .content');
+
   // Add has-content-details and has-content-sidebar classes to the content
   // article element if content details and/or a sidebar is present in the DOM.
-  if ($('.content-container > .content').length > 0) {
-    if ($('.content-container > .content-sidebar').length > 0) {
-      $('.content-container > .content').addClass('has-content-sidebar');
+  if (content.length > 0) {
+
+    var contentSidebar = $('.content-container > .content-sidebar');
+    var contentDetails = $('.content-container > .content-details');
+
+    // Add has-content-details and has-content-sidebar classes to the content
+    // article element if content details and/or a sidebar is present in the DOM.
+    if (contentSidebar.length > 0) {
+      content.addClass('has-content-sidebar');
     }
-    if ($('.content-container > .content-details').length > 0) {
-      $('.content-container > .content').addClass('has-content-details');
+    if (contentDetails.length > 0) {
+      content.addClass('has-content-details');
     }
+
+    // If we have a content sidebar, set the min-height of the content block to be
+    // equal to the height of the content sidebar, to ensure its not flowing out
+    // of bounds.
+    //
+    // TODO - This should be fixed in CSS eventually.
+    var minHeight = 0;
+    if (contentSidebar.length > 0) {
+      minHeight = contentSidebar.outerHeight();
+    }
+    if (contentDetails.length > 0 && contentDetails.outerHeight() > minHeight) {
+      minHeight = contentDetails.outerHeight()
+    }
+    if (minHeight > 0) {
+      content.css('min-height', minHeight + 15 + 'px');
+    }
+
   }
 
 });
