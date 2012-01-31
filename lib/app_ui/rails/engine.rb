850e2ce9fdd4592536ea05ae09810d7ff925e3e5
Trond Arve Nordheim <t@binarymarbles.com>
2012-01-31 22:07:24 +0100

    Reorganized stylesheets to work properly with the Rails 3.1+ pipeline. This has broken the demo in unspeakable ways, fix later.

--- lib/app_ui/rails/engine.rb
+++ lib/app_ui/rails/engine.rb
@@ -7,10 +7,17 @@ module AppUi
   module Rails
     class Engine < ::Rails::Engine
 
+      # Add the app-ui less framework to the Less load paths.
       initializer 'app-ui.setup', :after => 'less-rails.after.load_config_initializers', :group => :all do |app|
-        app.config.less.paths << File.join(config.root, 'vendor', 'assets', 'stylesheets')
+        app.config.less.paths << File.join(config.root, 'vendor', 'frameworks')
       end
 
+      # Add the jQuery UI image directory to the asset load path.
+      initializer 'app-ui.append_asset_path', :group => :all, :after => :append_assets_path do |app|
+        app.config.assets.paths << File.join(config.root, 'vendor', 'assets', 'stylesheets', 'app-ui', 'jquery-ui-images')
+      end
+
+      # Hook in the controller support logic.
       initializer 'app-ui.app_controller' do |app|
         ActiveSupport.on_load(:action_controller) do
 
