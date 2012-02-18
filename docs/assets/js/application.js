12523099c3525733e0d8fdf53db5336051e9bce8
Trond Arve Nordheim <t@binarymarbles.com>
2012-02-18 17:29:59 +0100

    Implemented the remaining parts of the bootstrap UI

---
+++ docs/assets/js/application.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  // tooltip demo
+  $('.tooltip-demo.well').tooltip({
+    selector: "a[rel=tooltip]"
+  })
+
+  $('.tooltip-test').tooltip()
+  $('.popover-test').popover()
+
+  // popover demo
+  $("a[rel=popover]").popover().click(function(e) {
+    e.preventDefault()
+  })
+
+  // button state demo
+  $('#fat-btn') .click(function () {
+    var btn = $(this)
+    btn.button('loading')
+    setTimeout(function () {
+      btn.button('reset')
+    }, 3000)
+  })
+  
+});
