34228e8cc4c17c6c5fcf2d585c958878f08c6deb
Trond Arve Nordheim <t@binarymarbles.com>
2012-03-08 06:56:55 +0100

    Making the icon_tag helper more complex, supporting text and color attributes

--- app/helpers/app_ui/icon_helper.rb
+++ app/helpers/app_ui/icon_helper.rb
@@ -6,10 +6,22 @@ module AppUi
     # Renders an icon tag for the specified icon.
     #
     # @param [ Symbol, String ] name The name of the icon.
+    # @param [ String ] text Optional text to append after the element.
     #
     # @return [ String ] The DOM element for rendering the icon.
-    def icon_tag(name)
-      content_tag(:i, '', class: "icon-#{name.to_s}")
+    def icon_tag(name, text = nil, *args)
+
+      options = args.extract_options!
+      options[:color] ||= :black
+
+      icon_classes = ["icon-#{name.to_s}"]
+      icon_classes << "icon-#{options[:color].to_s}" unless options[:color] == :black
+
+      value = ''
+      value << content_tag(:i, '', class: icon_classes.join(' '))
+      value << sanitize(" #{text}") unless text.blank?
+      value.html_safe
+
     end
 
   end
